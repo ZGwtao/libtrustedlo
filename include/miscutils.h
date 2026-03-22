89a99cbf939d560aab886d7e2627f413aac91f95
Guangtao Zhu <guangtaozhu@outlook.com>
2026-03-22 21:42:34 +1100

    miscutil: replace hand-written memcpy/memset with musllibc's alternative
    Signed-off-by: Guangtao Zhu <guangtaozhu@outlook.com>

--- include/miscutils.h
+++ include/miscutils.h
@@ -8,14 +8,14 @@
 
 
 void* tsldr_miscutil_memcpy(void* dest, const void* src, uint64_t n);
-void tsldr_miscutil_memset(void *dest, int value, uint64_t size);
+void* tsldr_miscutil_memset(void *dest, int value, uint64_t size);
 int tsldr_miscutil_memcmp(const unsigned char* s1, const unsigned char* s2, int n);
 int tsldr_miscutil_strcmp(const char* s1, const char* s2);
 
 
 void tsldr_miscutil_dbg_print(const char *format, ...);
 
-#if !defined(CONFIG_DEBUG_BUILD)
+#if 0
 #define TSLDR_DBG_PRINT(...) \
     do { tsldr_miscutil_dbg_print(__VA_ARGS__); } while (0)
 #else
